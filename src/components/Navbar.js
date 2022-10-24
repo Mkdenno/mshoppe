681866e905ad0001cd26a23a2ea12e04e7796b1e
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-10-24 07:59:57 +0300

    It will add login and register in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import "./navbar.css";
+import { Link } from "react-router-dom";
+
+
+const colorTxt = {
+  color: "white",
+  textDecoration: "none",
+};
 const NavBar = () => {
   return (
     <nav className=" container justify-around navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-          <h3><span className="text-danger">MoS</span>hopper</h3>
+        <h3>
+          <span className="text-danger">MoS</span>hopper
+        </h3>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -47,9 +56,43 @@ const NavBar = () => {
           </form>
         </div>
       </div>
+      <div className="my-4 btn">
+              <button
+                type="button"
+                className="btn btn-lg"
+                style={{
+                  border: "none",
+                  color: "white",
+                  backgroundColor: "#00BFFF",
+                  fontSize: "15px",
+                  fontWeight: "900",
+                }}
+              >
+                <Link to="/register" style={colorTxt}>
+                  Login
+                </Link>
+              </button>
+            </div> 
+            <div className="my-4 btn">
+              <button
+                type="button"
+                className="btn btn-lg"
+                style={{
+                  border: "none",
+                  color: "white",
+                  backgroundColor: "#00BFFF",
+                  fontSize: "15px",
+                  fontWeight: "900",
+                }}
+              >
+                <Link to="/register" style={colorTxt}>
+                  Register
+                </Link>
+              </button>
+            </div>    
       <span className="cartIcon">
-            <AiOutlineShoppingCart />
-          </span>
+        <AiOutlineShoppingCart />
+      </span>
     </nav>
   );
 };
