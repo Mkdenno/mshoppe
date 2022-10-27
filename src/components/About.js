a605a97c4e5dc0ad663a65e92ea0e0179592ec7c
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-10-27 14:08:17 +0300

    About

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className='row aboutus'>
             <div className='col-7'> 
                 <h1>About Us</h1>
-                <p>We've sorted these deals by price, so all you have to do is decide how much you want to spend. The specifications vary, too, so there's something for everyone whether you plan to use your laptop for gaming, work, school, or just need a portable option for checking your email.</p>
+                <p>MoShopper offers you a way to filter the best deals for electronics.We've sorted these deals by price, so all you have to do is decide how much you want to spend. The specifications vary, too, so there's something for everyone whether you plan to use your laptop for gaming, work, school, or just need a portable option for checking your email.</p>
             </div>
             <div className='col-5'>
                 <img src="./images/phone2.jpeg"/>
